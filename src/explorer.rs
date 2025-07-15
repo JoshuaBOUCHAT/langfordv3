24823d5188f15cf82fbfbb42baf15bf04b935e81
bouc0131 <joshua.bouchat-chapelet@etudiant.univ-reims.fr>
2025-07-15 23:07:30 +0200

    use btree

--- src/explorer.rs
+++ src/explorer.rs
@@ -1,50 +1,46 @@
-use std::{collections::HashMap, thread};
-
 use crate::langford::{LangfordSate, N};
+use std::collections::BTreeMap;
 
 pub struct Explorer {
-    states: HashMap<LangfordSate, u64>,
+    pub states: BTreeMap<LangfordSate, u64>,
 }
+
 impl Explorer {
     pub fn init_from_start() -> Self {
         Self {
-            states: HashMap::from([(LangfordSate::default(), 1)]),
+            states: BTreeMap::from([(LangfordSate::default(), 1)]),
         }
     }
 
     pub fn init_from_end() -> Self {
         Self {
-            states: HashMap::from([(LangfordSate::full_pattern(), 1)]),
+            states: BTreeMap::from([(LangfordSate::full_pattern(), 1)]),
         }
     }
+
     pub fn explore_down(self) -> Self {
         let new_patterns = self
             .states
             .into_iter()
-            .flat_map(|(p, nb)| (p.get_child_iter().map(move |p| (p.into_unique(), nb))));
-        let mut new_map = HashMap::new();
+            .flat_map(|(p, nb)| p.get_child_iter().map(move |p| (p.into_unique(), nb)));
+
+        let mut new_map = BTreeMap::new();
         for (pattern, nb) in new_patterns {
-            if let Some(total_nb) = new_map.get_mut(&pattern) {
-                *total_nb += nb;
-            } else {
-                new_map.insert(pattern, nb);
-            }
+            *new_map.entry(pattern).or_insert(0) += nb;
         }
 
         Explorer { states: new_map }
     }
+
     pub fn explore_up(self) -> Self {
         let new_patterns = self
             .states
             .into_iter()
-            .flat_map(|(p, nb)| (p.get_parent_iter().map(move |p| (p.into_unique(), nb))));
-        let mut new_map = HashMap::new();
+            .flat_map(|(p, nb)| p.get_parent_iter().map(move |p| (p.into_unique(), nb)));
+
+        let mut new_map = BTreeMap::new();
         for (pattern, nb) in new_patterns {
-            if let Some(total_nb) = new_map.get_mut(&pattern) {
-                *total_nb += nb;
-            } else {
-                new_map.insert(pattern, nb);
-            }
+            *new_map.entry(pattern).or_insert(0) += nb;
         }
 
         Explorer { states: new_map }
@@ -55,30 +51,20 @@ pub fn compute_langford() -> u64 {
     let mut start_explorer = Explorer::init_from_start();
     let mut end_explorer = Explorer::init_from_end();
 
-    let end_iter_count = N / 2;
-    let start_iter_count = N - end_iter_count;
+    let end_iter_count = 7;
+    let start_iter_count = 9;
 
-    let (start_result, end_result) = thread::scope(|s| {
-        let end_handle = s.spawn(move || {
-            for _ in 0..end_iter_count {
-                end_explorer = end_explorer.explore_up();
-            }
-            println!("end finished !");
-            end_explorer
-        });
+    for _ in 0..end_iter_count {
+        end_explorer = end_explorer.explore_up();
+    }
+    println!("end finished size: {}", end_explorer.states.len());
 
-        let start_handle = s.spawn(move || {
-            for _ in 0..start_iter_count {
-                start_explorer = start_explorer.explore_down();
-            }
-            start_explorer
-        });
+    for _ in 0..start_iter_count {
+        start_explorer = start_explorer.explore_down();
+    }
+    println!("start finished size: {}", start_explorer.states.len());
 
-        (
-            start_handle.join().expect("start thread panicked"),
-            end_handle.join().expect("end thread panicked"),
-        )
-    });
+    let (start_result, end_result) = (start_explorer, end_explorer);
 
     let mut total_count = 0;
     for (state, nb1) in start_result.states {
